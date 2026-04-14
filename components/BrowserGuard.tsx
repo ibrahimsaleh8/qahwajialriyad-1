f847685a493e41bfc64c22caf728d95744a7551a
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-04-14 11:17:15 +0200

    add more browsers

--- components/BrowserGuard.tsx
+++ components/BrowserGuard.tsx
@@ -12,20 +12,114 @@ export default function BrowserGuard() {
       const isChromeUA = /Chrome/.test(ua);
       const isGoogleVendor = vendor === "Google Inc.";
 
-      // block known browsers
-      const isEdge = /Edg/.test(ua);
-      const isOpera = /OPR/.test(ua);
+      // --- Chromium-based browsers ---
+      const isEdge = /Edg\//.test(ua);
+      const isOpera = /OPR\//.test(ua) || /Opera/.test(ua);
       const isUC = /UCBrowser/.test(ua);
+      const isSamsungBrowser = /SamsungBrowser/.test(ua);
+      const isYandex = /YaBrowser/.test(ua);
+      const isCocCoc = /coc_coc_browser/i.test(ua);
+      const isVivaldi = /Vivaldi/.test(ua);
+      const isBraveBrand = /Brave/.test(ua);
+      const isWhaleBrowser = /Whale/.test(ua);
+      const isDuckDuckGo = /DuckDuckGo/.test(ua);
+      const isArc = /Arc\//.test(ua);
+      const isThornbird = /Thornbird/.test(ua);
+      const isSleipnir = /Sleipnir/.test(ua);
+      const isAvast = /Avast\//.test(ua) || /AVG\//.test(ua);
+      const is360Browser =
+        /QIHU/.test(ua) || /360Browser/.test(ua) || /360SE/.test(ua);
+      const isBaidu = /BIDUBrowser/.test(ua) || /baiduboxapp/i.test(ua);
+      const isQQ = /QQBrowser/.test(ua) || /MQQBrowser/.test(ua);
+      const isSogou = /SogouMobileBrowser/.test(ua) || /MetaSr/.test(ua);
+      const isMaxthon = /Maxthon/.test(ua) || /MxBrowser/.test(ua);
+      const isMiuiBrowser = /MiuiBrowser/.test(ua);
+      const isHuaweiBrowser = /HuaweiBrowser/.test(ua);
+      const isOppoBrowser = /OPPO/.test(ua) || /HeyTapBrowser/.test(ua);
+      const isVivoBrowser = /VivoBrowser/.test(ua);
+      const isOnePlus = /OnePlus/.test(ua);
+      const isSzBrowser = /SZBrowser/.test(ua);
+      const isMZBrowser = /MZBrowser/.test(ua);
+      const isIridium = /Iridium\//.test(ua);
+      const isEpic = /Epic\//.test(ua);
+      const isCentBrowser = /CentBrowser/.test(ua);
+      const isGreenBrowser = /GreenBrowser/.test(ua);
+      const isSlimjet = /Slimjet/.test(ua);
+      const isTorch = /Torch\//.test(ua);
+      const isComodo = /Comodo/.test(ua) || /Dragon\//.test(ua);
+      const isSRWare = /Iron\//.test(ua);
+      const isCoolNovo = /CoolNovo/.test(ua);
+      const isRockMelt = /RockMelt/.test(ua);
+      const isFlock = /Flock\//.test(ua);
 
-      // detect Brave
+      // --- Non-Chromium browsers ---
+      const isFirefox =
+        /Firefox\//.test(ua) || /FxiOS/.test(ua) || /Fennec/.test(ua);
+      const isSafari =
+        /Safari\//.test(ua) && /Apple Computer/.test(vendor) && !isChromeUA;
+      const isIE = /Trident\//.test(ua) || /MSIE/.test(ua);
+      const isSeaMonkey = /SeaMonkey\//.test(ua);
+      const isPaleMoon = /PaleMoon\//.test(ua);
+      const isWaterfox = /Waterfox\//.test(ua);
+      const isTor = /TorBrowser/.test(ua) || /Tor\//.test(ua);
+      const isBasalt = /Basilisk\//.test(ua);
+      const isIceCat = /IceCat\//.test(ua);
+      const isIceWeasel = /Iceweasel\//.test(ua);
+      const isGecko =
+        /Gecko\//.test(ua) && !/Chrome/.test(ua) && !/Safari/.test(ua);
+      const isNetscape = /Netscape\//.test(ua);
+      const isKonqueror = /Konqueror/.test(ua);
+      const isLynx = /Lynx\//.test(ua);
+      const isLinks = /Links \(/.test(ua);
+      const isW3m = /w3m\//.test(ua);
+      const isElinks = /ELinks\//.test(ua);
+      const isLibreWolf = /LibreWolf\//.test(ua);
+      const isGnomeWeb = /Epiphany\//.test(ua);
+      const isMidori = /Midori\//.test(ua);
+      const isQuteBrowser = /qutebrowser/.test(ua);
+      const isDillo = /Dillo\//.test(ua);
+      const isNetSurf = /NetSurf\//.test(ua);
+      const isK_Meleon = /K-Meleon\//.test(ua);
+      const isLunascape = /Lunascape/.test(ua);
+      const isFalkon = /Falkon\//.test(ua);
+      const isOtter = /Otter\//.test(ua);
+
+      // --- Brave via API ---
       const isBrave =
+        isBraveBrand ||
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (navigator as any).brave && (await (navigator as any).brave.isBrave());
+        ((navigator as any).brave &&
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (await (navigator as any).brave.isBrave()));
 
-      // detect WebView (Android / in-app browsers)
-      const isWebView = /wv/.test(ua) || /Android.*Version\/[\d.]+/.test(ua);
+      // --- In-app / WebView browsers ---
+      const isWebView =
+        /wv/.test(ua) ||
+        /Android.*Version\/[\d.]+/.test(ua) ||
+        /FB_IAB/.test(ua) ||
+        /FBAN/.test(ua) ||
+        /FBAV/.test(ua) ||
+        /Instagram/.test(ua) ||
+        /Twitter/.test(ua) ||
+        /Line\//.test(ua) ||
+        /KAKAOTALK/.test(ua) ||
+        /Snapchat/.test(ua) ||
+        /Pinterest/.test(ua) ||
+        /TikTok/.test(ua) ||
+        /WhatsApp/.test(ua) ||
+        /Telegram/.test(ua) ||
+        /WeChat/.test(ua) ||
+        /MicroMessenger/.test(ua) ||
+        /LinkedInApp/.test(ua) ||
+        /GSA\//.test(ua) || // Google Search App
+        /DomainApp/.test(ua) ||
+        /Musical_ly/.test(ua) ||
+        /YahooMobile/.test(ua) ||
+        /Mail\//.test(ua) || // Apple Mail webview
+        /Outlook/.test(ua) ||
+        /com\.apple\.mobilenotes/.test(ua);
 
-      // check chromium object
+      // check chrome object exists
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const hasChromeObject = !!(window as any).chrome;
 
@@ -36,7 +130,67 @@ export default function BrowserGuard() {
         !isEdge &&
         !isOpera &&
         !isUC &&
+        !isSamsungBrowser &&
+        !isYandex &&
+        !isCocCoc &&
+        !isVivaldi &&
         !isBrave &&
+        !isWhaleBrowser &&
+        !isDuckDuckGo &&
+        !isArc &&
+        !isThornbird &&
+        !isSleipnir &&
+        !isAvast &&
+        !is360Browser &&
+        !isBaidu &&
+        !isQQ &&
+        !isSogou &&
+        !isMaxthon &&
+        !isMiuiBrowser &&
+        !isHuaweiBrowser &&
+        !isOppoBrowser &&
+        !isVivoBrowser &&
+        !isOnePlus &&
+        !isSzBrowser &&
+        !isMZBrowser &&
+        !isIridium &&
+        !isEpic &&
+        !isCentBrowser &&
+        !isGreenBrowser &&
+        !isSlimjet &&
+        !isTorch &&
+        !isComodo &&
+        !isSRWare &&
+        !isCoolNovo &&
+        !isRockMelt &&
+        !isFlock &&
+        !isFirefox &&
+        !isSafari &&
+        !isIE &&
+        !isSeaMonkey &&
+        !isPaleMoon &&
+        !isWaterfox &&
+        !isTor &&
+        !isBasalt &&
+        !isIceCat &&
+        !isIceWeasel &&
+        !isGecko &&
+        !isNetscape &&
+        !isKonqueror &&
+        !isLynx &&
+        !isLinks &&
+        !isW3m &&
+        !isElinks &&
+        !isLibreWolf &&
+        !isGnomeWeb &&
+        !isMidori &&
+        !isQuteBrowser &&
+        !isDillo &&
+        !isNetSurf &&
+        !isK_Meleon &&
+        !isLunascape &&
+        !isFalkon &&
+        !isOtter &&
         !isWebView;
 
       if (!isRealChrome) {
