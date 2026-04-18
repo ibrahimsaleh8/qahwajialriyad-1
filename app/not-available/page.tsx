7b65efc2e39aec84b55a266570e247acb9546a0f
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-04-18 16:27:04 +0200

    update NotAvailablePage

--- app/not-available/page.tsx
+++ app/not-available/page.tsx
@@ -2,19 +2,16 @@ export default function NotAvailablePage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-gray-50 to-gray-200 px-4">
       <div className="max-w-md w-full bg-white shadow-xl rounded-2xl p-8 text-center">
-        <div className="text-5xl mb-2">🇸🇦</div>
-        <div className="text-4xl mb-4">🚫</div>
-
         <h1 className="text-2xl font-bold text-gray-800 mb-3">
-          الخدمة غير متاحة
+          قهوجيين وصبابين قهوة الرياض
         </h1>
 
-        <p className="text-gray-600 mb-6 leading-relaxed">
-          نعتذر، هذا الموقع متاح حاليًا فقط داخل{" "}
-          <span className="font-semibold text-black">
-            المملكة العربية السعودية
-          </span>
-          .
+        <p
+          className="text-gray-600 mb-6 leading-relaxed"
+          style={{
+            direction: "ltr",
+          }}>
+          +966 554203782
         </p>
       </div>
     </div>
